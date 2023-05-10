25ab0b1fcca49163dfe1022d857bc165a1244f84
MECVxD <mauricioecovarrubias@hotmail.com>
2023-05-09 23:54:30 -0600

    First commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./style.css";
+// import "./topics/01-basic-types";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `Hola Mundo`;
+
+console.log("Hola Mundo");
+
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
